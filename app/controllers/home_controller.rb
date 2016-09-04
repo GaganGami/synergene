80740b1a28cac0d27d20198a290dd992ec0d0722
Gagan Gami <gagangami@gmail.com>
2016-09-05 04:15:10 +0530

    fix mailer issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -29,10 +29,11 @@ class HomeController < ApplicationController
   end
 
   def contact_us
-    @inquiry = Inquiry.new(params[:inquiry])
+    inquiry_params = {:email => params[:inquiry][:email], :subject => params[:inquiry][:subject], :content => params[:inquiry][:content], :name => params[:inquiry][:name]}
+    @inquiry = Inquiry.new(inquiry_params)
     if @inquiry.save!
-      ContactMailer.send_contact_us(@inquiry).deliver
-      ContactMailer.send_contact_confirm(@inquiry).deliver
+      ContactMailer.send_contact_us(inquiry_params).deliver
+      ContactMailer.send_contact_confirm(inquiry_params).deliver
     end
   end
 
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,12 +2,12 @@ class ContactMailer < ApplicationMailer
 	default to: "gagangami@gmail.com"
 
   def send_contact_us(contact_params)
-    @inquiry = contact_params
-    mail(from: @inquiry[:email], subject: "Inquiry Request : #{@inquiry[:subject]}")
+    @inquiry_params = contact_params
+    mail(from: @inquiry_params[:email], subject: "Inquiry Request : #{@inquiry_params[:subject]}")
   end
 
   def send_contact_confirm(contact_params)
-    @inquiry = contact_params
-    mail(to: @inquiry[:email], subject: "Inquiry Request", from: "info@synergene.com")
+    @inquiry_params = contact_params
+    mail(to: @inquiry_params[:email], subject: "Inquiry Request", from: "info@synergene.com")
   end
 end
