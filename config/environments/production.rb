b59903086b4baad8db06dacd2d2a451cbd97d06d
Gagan Gami <gagangami@gmail.com>
2016-09-06 00:09:01 +0530

    production file added assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,9 @@ Synergene1::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   # config.serve_static_assets = false
-  config.serve_static_files = false
+  config.serve_static_files = true
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+  config.assets.compile = false
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
